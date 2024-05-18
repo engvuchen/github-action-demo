b905859e941636789cb783973cd36d182ede2e99
HGJ <58281113+HuangGuanJin@users.noreply.github.com>
2024-05-18 15:52:16 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,8 @@ import HelloWorld from './components/HelloWorld.vue';
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Hello!" />
+      <HelloWorld msg="Test111" />
+      <HelloWorld msg="Test222" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
